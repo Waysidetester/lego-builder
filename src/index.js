1f209911810237443f4816f5a6e9bbb2a1e397dd
WaysideTester <dylanmurray211@outlook.com>
2018-11-10 13:24:02 -0600

    created ranomize button

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import 'bootstrap';
 import './index.scss';
 import savedCharacters from './javascripts/components/savedCreations/savedCharacters';
 import legoCharacters from './javascripts/components/legoCreations/legoCharacter';
+import rando from './javascripts/components/randomizer/rando';
 
 const init = () => {
   legoCharacters.displayAll();
   $('document').ready(legoCharacters.timedNamer);
   $('body').on('click', legoCharacters.timedNamer);
   $('#save').on('click', savedCharacters.saveAction);
+  rando.randomizeButton();
 };
 
 
--- src/javascripts/components/randomizer/rando.js
+++ src/javascripts/components/randomizer/rando.js
@@ -1,6 +1,23 @@
+import $ from 'jquery';
+
 const randomNumGenerator = (min, max) => {
   const result = Math.floor(Math.random() * (max - min + 0.9)) + min;
   return result;
 };
 
-export default { randomNumGenerator };
+const randomizeButton = () => {
+  $('#randomize').on('click', () => {
+    const imgSelector = $('.carousel-inner');
+    $.each(imgSelector, (i, bodyParts) => {
+      const partsArray = bodyParts.children;
+      $(partsArray).removeClass('active');
+      let newIndex = randomNumGenerator(0, partsArray.length);
+      while (newIndex >= partsArray.length) {
+        newIndex = randomNumGenerator(0, partsArray);
+      }
+      $(partsArray[newIndex]).addClass('active');
+    });
+  });
+};
+
+export default { randomNumGenerator, randomizeButton };
